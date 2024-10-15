c74be66babb95580ac31ab4106276a3d7efd856c
caiorodriveira <rodrigocaio2775@gmail.com>
2024-10-14 22:13:32 -0300

    Definindo retorno com mockito

--- src/test/java/br/sc/senai/service/UsuarioServiceTest.java
+++ src/test/java/br/sc/senai/service/UsuarioServiceTest.java
@@ -1,5 +1,6 @@
 package br.sc.senai.service;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
@@ -8,17 +9,38 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import br.sc.senai.domain.Usuario;
+import br.sc.senai.domain.builder.UsuarioBuilder;
 import br.sc.senai.repository.UsuarioRepository;
 
 public class UsuarioServiceTest {
 	private UsuarioService usuarioService;
 	
 	@Test
+	public void deveRetornarEmptyQuandoUsuarioInexistente() {
+		UsuarioRepository usuarioRepository = Mockito.mock(UsuarioRepository.class);
+		usuarioService = new UsuarioService(usuarioRepository);
+		
+		/*
+		 * quando chamar o método dentro do when (getUsuarioByLogin) retorne vazio
+		 * Nesse caso é redundante pq a classe ja retorna um empty por padrão
+		 */
+		Mockito.when(usuarioRepository.getUsuarioByLogin("vazio@vazio.com")).thenReturn(Optional.empty());
+		
+		//obedencendo regra acima
+		Optional<Usuario> usuario = usuarioService.getUsuarioByLogin("teste@teste.com");
+		assertTrue(usuario.isEmpty());
+	}
+	
+	@Test
 	public void deveRetornarUsuarioPorEmail() {
 		UsuarioRepository usuarioRepository = Mockito.mock(UsuarioRepository.class);
 		usuarioService = new UsuarioService(usuarioRepository);
 		
-		Optional<Usuario> usuario = usuarioService.getUsuarioByLogin("teste@teste.com");
-		assertTrue(usuario.isEmpty());
+		Mockito.when(usuarioRepository.getUsuarioByLogin("usuario@usuario.com"))
+		.thenReturn(Optional.of(UsuarioBuilder.umUsuario().build()));
+		
+		//obedencendo regra acima
+		Optional<Usuario> usuario = usuarioService.getUsuarioByLogin("usuario@usuario.com");
+		assertFalse(usuario.isEmpty());
 	}
 }
