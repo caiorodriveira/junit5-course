cb77e83ee6c7693a4ccc1692fda2b6e068a20434
caiorodriveira <rodrigocaio2775@gmail.com>
2024-11-17 12:13:20 -0300

    parameterizedtest prática notnull

--- src/test/java/br/sc/senai/service/TransacaoServiceTest.java
+++ src/test/java/br/sc/senai/service/TransacaoServiceTest.java
@@ -6,15 +6,24 @@ import static br.sc.senai.domain.builder.UsuarioBuilder.umUsuario;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
+
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.sc.senai.domain.Conta;
 import br.sc.senai.domain.Transacao;
+import br.sc.senai.exception.NotNullException;
 import br.sc.senai.repository.TransacaoDao;
 
 @ExtendWith(MockitoExtension.class)
@@ -50,4 +59,30 @@ public class TransacaoServiceTest {
 		
 	}
 	
+	@ParameterizedTest(name = "{6} - {1}")
+	@MethodSource(value = "cenariosObrigatorios")
+	public void deveValidarCamposObrigatoriosAoSalvar(Long id, String descricao, Double valor, LocalDateTime data, Conta conta, Boolean status, String errorMessage) {
+		Transacao transacaoToSave = umaTransacao()
+										.comId(id)
+										.comDescricao(descricao)
+										.comValor(valor)
+										.comData(data)
+										.comConta(conta)
+										.comStatus(status)
+									.build();
+		
+		String exMessage = assertThrows(NotNullException.class,  () -> transacaoService.salvar(transacaoToSave)).getMessage();
+		
+		assertEquals(errorMessage, exMessage);
+		
+	}
+	
+	static Stream<Arguments> cenariosObrigatorios() {
+		return Stream.of(
+					Arguments.of(1L, null, 1D, LocalDateTime.now(), umaConta().build(), false, "Descrição"+NotNullException.NOT_NULL_MESSAGE),
+					Arguments.of(1L, "Transação Válida", null, LocalDateTime.now(), umaConta().build(), false, "Valor"+NotNullException.NOT_NULL_MESSAGE),
+					Arguments.of(1L, "Transação Válida", 1D, null, umaConta().build(), false, "Data"+NotNullException.NOT_NULL_MESSAGE),
+					Arguments.of(1L, "Transação Válida", 1D, LocalDateTime.now(), null, false, "Conta"+NotNullException.NOT_NULL_MESSAGE)
+				);
+	}
 }
